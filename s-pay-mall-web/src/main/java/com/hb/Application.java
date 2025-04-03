5c55278a5c1a49edb0bc20fe1a271ee617ed93c3
shall_zhao <shall_zhao@noreply.gitcode.com>
2025-04-03 23:08:45 +0800

    feat: init

---
+++ s-pay-mall-web/src/main/java/com/hb/Application.java
@@ -0,0 +1,18 @@
+package com.hb;
+
+import org.springframework.beans.factory.annotation.Configurable;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @description 启动入口
+ */
+@SpringBootApplication
+@Configurable
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class);
+    }
+
+}
